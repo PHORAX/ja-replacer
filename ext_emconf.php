1c696159a7734c53c12812054150ec7fb2c506b6
Felix Kopp <felix-source@phorax.com>
2016-10-22 16:33:11 +0200

    Initial
    Friedly fork for EXT: ja_replacer

---
+++ ext_emconf.php
@@ -0,0 +1,14 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Content Replacer',
+    'description' => 'Replaces string patterns from the page. You can use it to replace URLs for Content Delivery Network (CDN).',
+    'category' => 'fe',
+    'version' => '2.0.0',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearcacheonload' => 0,
+    'author' => 'Simon Schick',
+    'author_email' => 'simonsimcity@gmail.com',
+);
